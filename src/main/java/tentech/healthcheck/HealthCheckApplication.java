9bcb7b1468f028dd594fca10a03bb6430796537b
Zhorobekova <zhorobekova.dev@gmail.com>
2025-08-26 18:47:27 +0300

    create

---
+++ src/main/java/tentech/healthcheck/HealthCheckApplication.java
@@ -0,0 +1,13 @@
+package tentech.healthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tentech/healthcheck/HealthCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package tentech.healthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
