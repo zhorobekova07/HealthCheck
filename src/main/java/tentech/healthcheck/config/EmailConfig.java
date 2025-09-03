9a7756bc33c0efaa509d5ec9b1fe0b11c659bb00
Argen <derzkijcelik@gmail.com>
2025-09-03 19:59:16 +0300

    sending simple email

--- src/main/java/tentech/healthcheck/config/EmailConfig.java
+++ src/main/java/tentech/healthcheck/config/EmailConfig.java
@@ -11,19 +11,19 @@ import java.util.Properties;
 @Configuration
 @RequiredArgsConstructor
 public class EmailConfig {
-    @Bean
-    public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("argenabdykadyrov5@gmail.com");
-        mailSender.setPassword("gopr khwd oylr wqlc");
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-        return mailSender;
+//    @Bean
+//    public JavaMailSender javaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//        mailSender.setUsername("argenabdykadyrov5@gmail.com");
+//        mailSender.setPassword("gopr khwd oylr wqlc");
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+//        return mailSender;
     }
-}
+//}
 
