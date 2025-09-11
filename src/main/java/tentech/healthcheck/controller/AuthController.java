93c93f667bd32844dadffcd19f72127eea236ddb
Syimyk <syimyksadir@gmail.com>
2025-09-11 21:21:09 +0400

    fix login jwtUtil

--- src/main/java/tentech/healthcheck/controller/AuthController.java
+++ src/main/java/tentech/healthcheck/controller/AuthController.java
@@ -1,6 +1,5 @@
 package tentech.healthcheck.controller;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/tentech/healthcheck/security/jwt/JwtTokenUtil.java
+++ src/main/java/tentech/healthcheck/security/jwt/JwtTokenUtil.java
@@ -16,7 +16,8 @@ import java.util.function.Function;
 
 @Component
 public class JwtTokenUtil {
-    @Value("${jwt.secret}")
+//    @Value("${jwt.secret}")
+@Value("${jwt.secret}")
     private String secretKey;
     private final Long TOKEN_EXPIRATION = 24 * 7 * 60 * 60 * 1000L;
 
@@ -28,37 +29,68 @@ public class JwtTokenUtil {
                 .signWith(Keys.hmacShaKeyFor(Base64.getDecoder().decode(secretKey)), SignatureAlgorithm.HS256)
                 .compact();
     }
-    // генерация токен
-    public String generateToken(UserDetails userDetails) {
-        HashMap<String, Object> claims = new HashMap<>();
-        return createToken(claims, userDetails.getUsername());
-    }
+//    // генерация токен
+//    public String generateToken(UserDetails userDetails) {
+//        Map<String, Object> claims = new HashMap<>();
+//        return createToken(claims, userDetails.getUsername());
+//    }
+//
+//    private Claims getAllClaimsFromToken(String token) {
+//        return Jwts.parser()
+//                .setSigningKey(secretKey)
+//                .parseClaimsJws(token).getBody();
+//    }
+//
+//    private <T> T getClaimFromToken(String token, Function<Claims, T> function) {
+//        final Claims claims = getAllClaimsFromToken(token);
+//        return function.apply(claims);
+//    }
+//    // убактысын карайт
+//    public Date getExpirationDateFromToken(String token) {
+//        return getAllClaimsFromToken(token).getExpiration();
+//    }
+//
+//    private Boolean isTokenExpired(String token) {
+//        final Date expiration = getExpirationDateFromToken(token);
+//        return expiration.before(new Date());
+//    }
+//
+//    public String getUsernameFromToken(String token) {
+//        return getAllClaimsFromToken(token).getSubject();
+//    }
+//
+//    public Boolean tokenValidation(String token,UserDetails userDetails) {
+//        final String username = getUsernameFromToken(token);
+//        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+//    }
 
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+
+    }
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser()
                 .setSigningKey(secretKey)
-                .parseClaimsJws(token).getBody();
+                .parseClaimsJws(token)
+                .getBody();
     }
-
-    private <T> T getClaimFromToken(String token, Function<Claims, T> function) {
+    private <T> T getClaimsFromToken(String token, Function<Claims, T> function) {
         final Claims claims = getAllClaimsFromToken(token);
         return function.apply(claims);
     }
-    // убактысын карайт
     public Date getExpirationDateFromToken(String token) {
-        return getAllClaimsFromToken(token).getExpiration();
+        return getClaimsFromToken(token,Claims::getExpiration);
     }
-
     private Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
-
     public String getUsernameFromToken(String token) {
-        return getAllClaimsFromToken(token).getSubject();
+        return getClaimsFromToken(token,Claims::getSubject);
     }
 
-    public Boolean tokenValidation(String token,UserDetails userDetails) {
+    public Boolean tokenValidation(String token, UserDetails userDetails) {
         final String username = getUsernameFromToken(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
