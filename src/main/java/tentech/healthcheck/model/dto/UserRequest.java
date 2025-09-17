bdbfcc08841dad61460f17f22c5923cf8d4f9810
Zhazira <zhaziraidea@gmail.com>
2025-09-17 12:26:18 +0300

    fix login and sign up дубль 676434

--- src/main/java/tentech/healthcheck/model/dto/UserRequest.java
+++ src/main/java/tentech/healthcheck/model/dto/UserRequest.java
@@ -1,12 +1,11 @@
-package tentech.healthcheck.model.dto;
-
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-public class UserRequest {
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-}
+//package tentech.healthcheck.model.dto;
+//
+//import lombok.Builder;
+//import lombok.Data;
+//
+//@Data
+//@Builder
+//public class UserRequest {
+//    private String firstName;
+//    private String lastName;
+//}
