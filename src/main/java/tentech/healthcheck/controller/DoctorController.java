1890c99b6bfa43ded3a9999740ef9f2cb9e9f894
Zhorobekova <zhorobekova.dev@gmail.com>
2025-09-23 15:07:48 +0300

    added swagger

--- src/main/java/tentech/healthcheck/controller/DoctorController.java
+++ src/main/java/tentech/healthcheck/controller/DoctorController.java
@@ -1,4 +1,7 @@
 package tentech.healthcheck.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Doctor Controller")
 public class DoctorController {
 }
--- src/main/java/tentech/healthcheck/controller/EmailController.java
+++ src/main/java/tentech/healthcheck/controller/EmailController.java
@@ -1,5 +1,7 @@
 package tentech.healthcheck.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tentech.healthcheck.model.dto.EmailRequest;
 import tentech.healthcheck.serviceImpl.EmailService;
-
+@Tag(name = "Email Controller")
 @RestController
 @RequestMapping("/api/email")
 public class EmailController {
@@ -15,6 +17,7 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
+    @Operation(summary = "Отправка письма" ,description = "Принимает EmailRequest (адрес, тема, текст) и возвращает статус операции")
     @PostMapping("/sendMail")
     public String
     sendMail(@RequestBody EmailRequest details) {
@@ -22,6 +25,7 @@ public class EmailController {
         return status;
     }
 
+    @Operation(summary = "Отправка письма с вложением" ,description = "Принимает EmailRequest (адрес, тема, текст, вложение) и возвращает статус операции")
     @PostMapping("/sendMailWithAttachment")
     public String sendMailWithAttachment(
             @RequestBody EmailRequest details) {
